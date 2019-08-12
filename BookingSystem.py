40388ce9659f9dc20fde7f304943be5d0d5ce842
CharlieHutty <46685705+CharlieHutty@users.noreply.github.com>
2019-08-12 13:56:17 +0100

    Add files via upload

--- BookingSystem.py
+++ BookingSystem.py
@@ -5,13 +5,90 @@
 # import modules
 import datetime as dt
 import csv
-import username
-import roomtobook
-import enddate
-import startdate
-import starttime
-import endtime
-import deletebookings
+
+
+def UserName():
+    while True:
+        global username     # Gloabal variable that can be passed through the functions
+        # Allow the user to enter their name
+        username = input("Enter your name: ").lower()
+        if username.isalpha():
+            break
+        else:
+            print("That is not a name")
+            print("Try again")
+
+
+def RoomToBook():
+    while True:
+        global roomtobook   # Gloabl variable, can be accessed throughout the program
+        # Allow the user to enter the room they would like to book
+        roomtobook = int(input("Enter the number of room you would like to book (1-4): "))  # Gets the users input
+        if roomtobook < 1 or roomtobook > 4:    # Makes sure that the users input is within the boundaries
+            print("Please enter a number between 1 and 4")  # Makes the user keep repeating the line until they are within the boundaries
+        else:
+            break   # Breaks out of the loop when the conditions are met
+
+
+
+def DateToBook():
+    global datestart
+    global dateend
+    while True:
+        # This is the date format in which we want the data entered in
+        dateformat = '%d/%m/%Y'
+        # Allow the user to input what date they would like to book the room
+        datestart = input("What date would you like the room to be booked? Type Date dd/mm/yyyy: ")
+        try:
+            datestart = dt.datetime.strptime(datestart, dateformat) # Checks to see if the format matches
+        except ValueError:
+            print("Incorrect format")   # Tells the user that the format is wrong
+            # Makes them try again
+        else:
+            datestart = datestart.strftime('%d/%m/%Y')  # This takes off the unnecessary parts
+            break # Carries on the program if no errors are found
+
+    while True:
+        # Allow the user to enter the date the booking will end
+        dateend = input("What date would you like the booking to end? Type Data dd/mm/yyyy: ")
+        try:
+            dateend = dt.datetime.strptime(dateend, dateformat)
+        except ValueError:
+            print("Incorrect format")
+        else:
+            dateend = dateend.strftime('%d/%m/%Y')  # This takes off the unnecessary parts
+            break
+
+def TimeToBook():
+    global timestart
+    global timeend
+    # The format we want the time in
+    timeformat = '%H%M'
+    timetoday = datetime.now().
+    while True:
+        # Allow the user to enter the time they would like to book the room
+        timestart = input("What time would you like the booking to start? Enter in 24-hour format ")
+        try:
+            timestart = dt.datetime.strptime(timestart, timeformat)     # Checks to make sure that the data is in the correct format
+        except ValueError:
+            print("Incorrect format")
+        else:
+            if timestart.time() < today.time():
+
+            timestart = timestart.strftime('%H%M')  # This takes off the unnecessary parts
+            break
+    # For the end time of the meeting
+    while True:
+        # Allow the user to enter the time they would like the booking to end
+        timeend = input("What time would you like the booking to end? ")
+        try:
+            timeend = dt.datetime.strptime(timeend, timeformat)
+        except ValueError:
+            print("Incorrect format")
+        else:
+            if
+            timeend = timeend.strftime('%H%M')  # This takes off the unnecessary parts
+            break
 
 
 def CheckBooking():
@@ -31,16 +108,16 @@ def CheckBooking():
             print("Successful")     # Outputs to the user that their booking has been successful
             FileHandle()    # Runs the function to put the user details into the csv
 
-def FileHandle():
 
+
+
+def FileHandle():
     # Open the csv
     with open('ExistingBookings.csv', 'a') as ExistingBookings:
         ExistingBookings = csv.writer(ExistingBookings, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
         # Writes the users input to the CSV
         ExistingBookings.writerow([username, roomtobook, datestart, dateend, timestart, timeend])
-
-        print('\n Name: ', username, '\n', 'Room Booked: ', roomtobook, '\n', 'Start Date: ', datestart, '\n', 'End Date: ', dateend, '\n', 'Start Time: ', timestart, '\n', 'End Time: ', timeend)
 
 def DeleteBookings():
     with open('ExistingBookings.csv') as ExistingBookings:
@@ -61,14 +138,12 @@ def DeleteBookings():
 
 
 if __name__ == "__main__":
-    username = username.UserName()
-    roomtobook = roomtobook.RoomToBook()
-    datestart = startdate.startdate()
-    dateend = startdate.enddate()
-    timestart = starttime.StartTime()
-    timeend = endtime.endtime()
+    UserName()
+    RoomToBook()
+    DateToBook()
+    TimeToBook()
     CheckBooking()
-    # deletebookings.DeleteBookings()
+    # DeleteBookings()
 
 
 
