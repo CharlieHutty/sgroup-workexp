af6320d2da04a1caebaaf2b0b27a5b4433f7fe04
CharlieHutty <46685705+CharlieHutty@users.noreply.github.com>
2019-08-12 15:51:36 +0100

    Delete username.py

--- username.py
+++
@@ -1,9 +0,0 @@
-def UserName():
-    while True:
-        # Allow the user to enter their name
-        username = input("Enter your name: ").lower()
-        if username.isalpha():
-            return username
-        else:
-            print("That is not a name")
-            print("Try again")
